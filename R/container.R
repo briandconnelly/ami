37e09d50060c12bc8e66934d0b2235bb3531879c
Brian Connelly <bconnelly@ookla.com>
2023-01-01 10:14:47 -0800

    Add `using_podman_container()`

--- R/container.R
+++ R/container.R
@@ -7,7 +7,7 @@
 #' @examples
 #' using_container()
 using_container <- function() {
-  using_docker_container()
+  using_docker_container() || using_podman_container()
 }
 
 
@@ -18,4 +18,14 @@ using_container <- function() {
 using_docker_container <- function() {
   # This may not work 100%
   file.exists("/.dockerenv")
+}
+
+
+#' @rdname container
+#' @export
+#' @examples
+#' using_podman_container()
+using_podman_container <- function() {
+  # This may not work 100%
+  file.exists("/run/.containerenv")
 }
