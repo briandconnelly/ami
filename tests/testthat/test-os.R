564ae37b6e011afa88762ff5384a3059308d3923
Brian Connelly <bconnelly@ookla.com>
2023-04-08 17:11:34 -0700

    Remove duplicated tests

--- tests/testthat/test-os.R
+++ tests/testthat/test-os.R
@@ -6,11 +6,6 @@ test_that("using_os() works", {
   expect_equal(current_os == "solaris", using_solaris())
   expect_equal(current_os == "windows", using_windows())
 
-  expect_equal(current_os == "linux", using_linux())
-  expect_equal(current_os == "darwin", using_macos())
-  expect_equal(current_os == "solaris", using_solaris())
-  expect_equal(current_os == "windows", using_windows())
-
   expect_error(using_os("mac"))
   expect_error(using_os(""))
   expect_error(using_os(NA_character_))
