12493d709fa8a9df82e8f5bb7576b11543bef68b
Brian Connelly <bconnelly@ookla.com>
2023-04-08 17:10:48 -0700

    Add tests for `test_os()`

--- tests/testthat/test-os.R
+++ tests/testthat/test-os.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("using_os() works", {
+  current_os <- tolower(Sys.info()[["sysname"]])
+
+  expect_equal(current_os == "linux", using_linux())
+  expect_equal(current_os == "darwin", using_macos())
+  expect_equal(current_os == "solaris", using_solaris())
+  expect_equal(current_os == "windows", using_windows())
+
+  expect_equal(current_os == "linux", using_linux())
+  expect_equal(current_os == "darwin", using_macos())
+  expect_equal(current_os == "solaris", using_solaris())
+  expect_equal(current_os == "windows", using_windows())
+
+  expect_error(using_os("mac"))
+  expect_error(using_os(""))
+  expect_error(using_os(NA_character_))
+  expect_error(using_os(NULL))
+  expect_error(using_os(c("macos", "windows")))
 })
