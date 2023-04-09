0eb2d6b12501fc8da383b3995011fa53838ace7f
Brian Connelly <bconnelly@ookla.com>
2023-04-08 17:14:26 -0700

    Add tests for `using_kubernetes()`

--- tests/testthat/test-container.R
+++ tests/testthat/test-container.R
@@ -12,3 +12,15 @@ test_that("using_docker_container() works as expected", {
 test_that("using_podman_container() works as expected", {
   expect_equal(using_podman_container(), file.exists("/run/.containerenv"))
 })
+
+test_that("using_kubernetes() works as expected", {
+  withr::with_envvar(
+    new = c("KUBERNETES_SERVICE_HOST" = "true"),
+    expect_true(using_kubernetes())
+  )
+
+  withr::with_envvar(
+    new = c("KUBERNETES_SERVICE_HOST" = NA),
+    expect_false(using_kubernetes())
+  )
+})
