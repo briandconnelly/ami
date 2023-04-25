bf984d697d45be1aa6522d5eac9258d8f5a3c815
Brian Connelly <bconnelly@ookla.com>
2023-04-25 07:07:52 -0700

    Add `using_rstudio_dark_theme()`, more RStudio documentation

--- R/rstudio.R
+++ R/rstudio.R
@@ -1,5 +1,11 @@
 #' @rdname rstudio
 #' @title RStudio environments
+#' @description
+#' These functions enable you to determine whether code is being run in the
+#' presence of various features of the RStudio IDE.
+#'
+#' `using_rstudio()` determines whether code is being run in
+#' RStudio
 #'
 #' @return A logical value
 #' @export
@@ -11,9 +17,23 @@ using_rstudio <- function() {
 }
 
 #' @rdname rstudio
+#' @description `using_rstudio_jobs()` determines whether code is running as
+#' an [RStudio Job](https://docs.posit.co/ide/user/ide/guide/tools/jobs.html)
+#'
 #' @export
 #' @examples
 #' using_rstudio_jobs()
 using_rstudio_jobs <- function() {
   using_envvar("RSTUDIOAPI_IPC_REQUESTS_FILE")
+}
+
+#' @rdname rstudio
+#' @description `using_rstudio_dark_theme()` determines whether a dark theme is
+#' being used
+#'
+#' @export
+#' @examples
+#' using_rstudio_dark_theme()
+using_rstudio_dark_theme <- function() {
+  isTRUE(rstudioapi::getThemeInfo()$dark)
 }
