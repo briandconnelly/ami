6603647fdcda418449d9aa75f8d997e89edfd04d
Brian Connelly <bconnelly@ookla.com>
2023-04-30 16:49:43 -0700

    Add `using_r_version()`

--- R/r.R
+++ R/r.R
@@ -1,5 +1,20 @@
 #' @rdname r
 #' @title R session information
+#' @description
+#' Get information about the R environment being used.
+#'
+#' `using_r_version()` determines whether or not a particular version of R is
+#' being used.
+#' @param ver Version string
+#' @export
+#' @examples
+#' using_r_version(ver = "4.3.0")
+using_r_version <- function(ver) {
+  getRversion() == ver
+}
+
+
+#' @rdname r
 #' @description `using_interactive_session()` determines whether or not R is
 #' being run interactively. Under the hood, [rlang::is_interactive] is used.
 #' @export
--- tests/testthat/test-r.R
+++ tests/testthat/test-r.R
@@ -1,3 +1,21 @@
+test_that("using_r_version() works as expected", {
+  expect_true(using_r_version(getRversion()))
+  expect_true(is.logical(using_r_version(getRversion())))
+
+  expect_length(
+    using_r_version(c("1.2.3", "4.5.6", as.character(getRversion()))),
+    3
+  )
+
+  expect_true(
+    any(using_r_version(c("1.2.3", "4.5.6", as.character(getRversion()))))
+  )
+  expect_false(
+    all(using_r_version(c("9.2.3", "4.5.6", as.character(getRversion()))))
+  )
+})
+
 test_that("using_interactive_session() works as expected", {
   expect_equal(using_interactive_session(), rlang::is_interactive())
+  expect_true(is.logical(using_interactive_session()))
 })
