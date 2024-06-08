391ad4ebf3359f7d1fa0d9162f45abf57f68354a
Lluís <lluis.revilla@gmail.com>
2024-06-08 18:09:18 +0200

    Update tests

--- tests/testthat/test-bioconductor.R
+++ tests/testthat/test-bioconductor.R
@@ -1,11 +1,11 @@
 test_that("on_bioconductor() works as expected", {
   withr::with_envvar(
-    new = c("BBS_HOME" = "true"),
+    new = c("IS_BIOC_BUILD_MACHINE" = "true"),
     expect_true(on_bioconductor())
   )
 
   withr::with_envvar(
-    new = c("BBS_HOME" = NA),
+    new = c("IS_BIOC_BUILD_MACHINE" = NA),
     expect_false(on_bioconductor())
   )
 })
