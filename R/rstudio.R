4250ba39327fd70247a5da767536416d3565e994
Brian Connelly <bconnelly@ookla.com>
2024-10-24 07:58:23 -0700

    Add `mode` argument to check for specific RStudio environments

--- R/rstudio.R
+++ R/rstudio.R
@@ -1,19 +1,53 @@
 #' @rdname rstudio
 #' @title RStudio environments
+#' @param mode Optional argument specifying whether RStudio is being used in
+#'   `"desktop"` mode or in `"server"`/`"workbench"` mode.
 #' @description
 #' These functions enable you to determine whether code is being run in the
 #' presence of various features of the RStudio IDE and other Posit products.
 #'
-#' `using_rstudio()` determines whether code is being run in
-#' RStudio
+#' `using_rstudio()` determines whether code is being run in RStudio.
+#' `using_rstudio_desktop()`, `using_rstudio_server()`, and
+#' `using_rstudio_workbench()` are helpers to determine whether those specific
+#' environments are being used.
 #'
 #' @return A logical value
 #' @export
 #'
 #' @examples
 #' using_rstudio()
-using_rstudio <- function() {
-  rstudioapi::isAvailable()
+using_rstudio <- function(mode = "any") {
+  mode <- rlang::arg_match(
+    arg = mode,
+    values = c("any", "desktop", "server", "workbench"),
+    multiple = FALSE
+  )
+
+  if (mode == "any") {
+    rstudioapi::isAvailable()
+  } else if (mode == "desktop") {
+    rstudioapi::isAvailable() && rstudioapi::getMode() == "desktop"
+  } else if (mode == "server" || mode == "workbench") {
+    rstudioapi::isAvailable() && rstudioapi::getMode() == "server"
+  }
+}
+
+#' @rdname rstudio
+#' @export
+using_rstudio_desktop <- function() {
+  using_rstudio(mode = "desktop")
+}
+
+#' @rdname rstudio
+#' @export
+using_rstudio_server <- function() {
+  using_rstudio(mode = "server")
+}
+
+#' @rdname rstudio
+#' @export
+using_rstudio_workbench <- function() {
+  using_rstudio(mode = "server")
 }
 
 #' @rdname rstudio
